6db9ddd1f0c246143889bba3951f9fdf1773f119
AbdullahJaspal <abdullahafzal4444@gmail.com>
2023-12-16 20:36:33 +0500

    New Notes

--- src/screens/home/addIdea/addIdea.js
+++ src/screens/home/addIdea/addIdea.js
@@ -1,5 +1,12 @@
 import React, {useRef, useState} from 'react';
-import {Image, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Topbar from '../../../components/Topbar';
 import {theme} from '../../../theme/theme';
 import RBSheet from 'react-native-raw-bottom-sheet';
@@ -26,84 +33,29 @@ const AddIdea = () => {
     theme.colors.neutral.white,
   );
   return (
-    <View style={{flex: 1, backgroundColor: 'white'}}>
+    <View style={{flex: 1, backgroundColor: backgroundColor}}>
       <Topbar type={'home'} />
-      <View
-        style={{
-          flexDirection: 'row',
-          width: '95%',
-          marginTop: 10,
-          alignSelf: 'center',
-        }}>
+      <View style={styles.titleCont}>
         <Image
           source={require('../../../assets/icons/bulb.png')}
-          style={{width: 30, height: 30, resizeMode: 'contain', marginTop: 5}}
+          style={styles.icon}
         />
-        <Text
-          style={{
-            fontSize: 32,
-            fontFamily: theme.fontFamily.bold,
-            width: '70%',
-          }}>
-          {' '}
-          New Product Ideas
-        </Text>
+        <Text style={styles.title}> New Product Ideas</Text>
       </View>
       <TextInput
         multiline
         placeholder="Start typing ideas"
-        style={{
-          width: '95%',
-          alignSelf: 'center',
-          marginTop: 10,
-          fontFamily: theme.fontFamily.regular,
-        }}
+        style={styles.input}
       />
-      <View
-        style={{
-          borderBottomWidth: 1,
-          borderColor: theme.colors.neutral.baseGrey,
-          marginTop: 20,
-          width: '95%',
-          alignSelf: 'center',
-        }}
-      />
-      <Text
-        style={{
-          fontSize: 14,
-          fontFamily: theme.fontFamily.regular,
-          width: '70%',
-          color: theme.colors.neutral.darkGrey,
-          marginTop: 10,
-          marginLeft: 10,
-        }}>
-        {' '}
-        Reminder set on 15/07/2021, 18:30
-      </Text>
+      <View style={styles.line} />
+      <Text style={styles.reminder}> Reminder set on 15/07/2021, 18:30</Text>
       <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
         {tags.map(item => {
           return (
             <TouchableOpacity
-              style={{
-                marginTop: 10,
-                marginLeft: 10,
-                backgroundColor: theme.colors.neutral.lightGrey,
-                padding: 3,
-                alignItems: 'center',
-                justifyContent: 'center',
-                borderRadius: 50,
-                paddingHorizontal: 8,
-              }}
+              style={styles.tagCont}
               onPress={() => rbsheet.current.open()}>
-              <Text
-                style={{
-                  fontSize: 14,
-                  fontFamily: theme.fontFamily.regular,
-                  color: theme.colors.neutral.darkGrey,
-                }}>
-                {' '}
-                {item}{' '}
-              </Text>
+              <Text style={styles.tagTxt}> {item} </Text>
             </TouchableOpacity>
           );
         })}
@@ -116,14 +68,7 @@ const AddIdea = () => {
         customStyles={{
           container: {},
         }}>
-        <View
-          style={{
-            backgroundColor: theme.colors.neutral.darkGrey,
-            borderRadius: 20,
-            alignSelf: 'flex-end',
-            marginRight: 10,
-            marginTop: 10,
-          }}>
+        <View style={styles.crossCont}>
           <Icon
             name="closecircle"
             type="ant-design"
@@ -133,38 +78,13 @@ const AddIdea = () => {
             }}
           />
         </View>
-        <Text
-          style={{
-            fontFamily: theme.fontFamily.regular,
-            marginLeft: 10,
-            color: theme.colors.neutral.darkGrey,
-          }}>
-          CHANGE BACKGROUND
-        </Text>
+        <Text style={styles.changeBg}>CHANGE BACKGROUND</Text>
         <View style={{flexDirection: 'row', alignItems: 'center'}}>
           {colors.map(item => {
             return (
-              <View
-                style={{
-                  width: 40,
-                  height: 40,
-                  borderRadius: 50,
-                  marginLeft: 10,
-                  marginTop: 10,
-                  borderWidth: backgroundColor === item ? 2 : 0,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  borderColor: theme.colors.neutral.darkGrey,
-                }}>
+              <View style={styles.colorCont(backgroundColor, item)}>
                 <TouchableOpacity
-                  style={{
-                    backgroundColor: item,
-                    width: 30,
-                    height: 30,
-                    borderRadius: 50,
-                    borderWidth: 1,
-                    borderColor: theme.colors.neutral.lightGrey,
-                  }}
+                  style={styles.color(backgroundColor, item)}
                   onPress={() => {
                     setBackgroundColor(item);
                   }}></TouchableOpacity>
@@ -172,17 +92,97 @@ const AddIdea = () => {
             );
           })}
         </View>
-        <View
-          style={{
-            borderBottomWidth: 0.5,
-            width: '95%',
-            alignSelf: 'center',
-            marginTop: 10,
-            borderColor: theme.colors.neutral.darkGrey,
-          }}></View>
+        <View style={styles.line} />
       </RBSheet>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainCont: {flex: 1, backgroundColor: 'white'},
+  titleCont: {
+    flexDirection: 'row',
+    width: '95%',
+    marginTop: 10,
+    alignSelf: 'center',
+  },
+  icon: {width: 30, height: 30, resizeMode: 'contain', marginTop: 5},
+  title: {
+    fontSize: 32,
+    fontFamily: theme.fontFamily.bold,
+    width: '70%',
+  },
+  input: {
+    width: '95%',
+    alignSelf: 'center',
+    marginTop: 10,
+    fontFamily: theme.fontFamily.regular,
+  },
+  line: {
+    borderBottomWidth: 1,
+    borderColor: theme.colors.neutral.baseGrey,
+    marginTop: 20,
+    width: '95%',
+    alignSelf: 'center',
+  },
+  reminder: {
+    fontSize: 14,
+    fontFamily: theme.fontFamily.regular,
+    width: '70%',
+    color: theme.colors.neutral.darkGrey,
+    marginTop: 10,
+    marginLeft: 10,
+  },
+  tagCont: {
+    marginTop: 10,
+    marginLeft: 10,
+    backgroundColor: theme.colors.neutral.lightGrey,
+    padding: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    paddingHorizontal: 8,
+  },
+  tagTxt: {
+    fontSize: 14,
+    fontFamily: theme.fontFamily.regular,
+    color: theme.colors.neutral.darkGrey,
+  },
+  crossCont: {
+    backgroundColor: theme.colors.neutral.darkGrey,
+    borderRadius: 20,
+    alignSelf: 'flex-end',
+    marginRight: 10,
+    marginTop: 10,
+  },
+  changeBg: {
+    fontFamily: theme.fontFamily.regular,
+    marginLeft: 10,
+    color: theme.colors.neutral.darkGrey,
+  },
+  colorCont: (color, item) => {
+    return {
+      width: 40,
+      height: 40,
+      borderRadius: 50,
+      marginLeft: 10,
+      marginTop: 10,
+      borderWidth: color === item ? 2 : 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderColor: theme.colors.neutral.darkGrey,
+    };
+  },
+  color: (color, item) => {
+    return {
+      backgroundColor: item,
+      width: 30,
+      height: 30,
+      borderRadius: 50,
+      borderWidth: 1,
+      borderColor: theme.colors.neutral.lightGrey,
+    };
+  },
+});
 
 export default AddIdea;
